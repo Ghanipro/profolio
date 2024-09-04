2b8e3b76c1e99a34cee9b8c8becc978f86e4e303
Mohammed Ghani <100181775+Ghanipro@users.noreply.github.com>
2024-09-04 11:57:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
-import Technologies from './components/technologies.jsx';
+import Technologies from './components/Technologies.jsx';
 import Projects from './components/Projects.jsx';
 import Contact from './components/Contact.jsx';
 function App(){
